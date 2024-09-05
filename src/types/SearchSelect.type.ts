65603f42c9ea3413fb2f8cfb2585fe0389be7863
AdetolaAremu <user@MACs-MacBook-Pro.local>
2024-09-05 18:19:35 +0100

    improvements: ability to reset and cleaned up the define props

---
+++ src/types/SearchSelect.type.ts
@@ -0,0 +1,14 @@
+export type Option<T = unknown> = { id: T; [key: string]: unknown }
+
+export interface searchSelectProps {
+  data: Option | any
+  placeholderName: string
+  displayKey: string
+  selectMax?: number | null
+  defaultValue?: Object | string[]
+  listBackgroundColor?: string
+  inputBorderColour?: string
+  inputFocusBorderColor?: string
+  primaryKey: string | number
+  modelValue: any[]
+}
