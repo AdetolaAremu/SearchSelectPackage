9dbb0e5a8d8062de3c0e26a292c57986c6b655de
AdetolaAremu <user@MACs-MacBook-Pro.local>
2024-10-01 21:43:53 +0100

    dynamic multi select init

---
+++ src/components/DynamicMultiSelect.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="_dynamic_container"></div>
+
+    <div>
+      <input class="_dynamic_search_input" type="text" />
+    </div>
+
+    <ul></ul>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+._dynamic_container {
+  background: white;
+  border: 1px solid gray;
+  width: 100%;
+  height: 35px;
+  border-radius: 0.5rem;
+}
+
+._dynamic_search_input {
+  width: 100%;
+  background: #f9fafb;
+  padding: 9px;
+}
+</style>
